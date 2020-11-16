80c41f426712127c7bfc1382a578de1b991a14a4
Aditeya Baral <aditeya.baral@gmail.com>
2020-11-16 16:31:07 +0530

    initial commit

--- init.py
+++ init.py
@@ -8,23 +8,20 @@ import cryptography
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext
 from pyspark.streaming import StreamingContext
-#from quiet_logs import quiet_logs
 
 
 sc = SparkContext("local", "fpl")
 sql_context = SQLContext(sc)
-ssc  = StreamingContext(sc, 1)
+ssc = StreamingContext(sc, 1)
 
-p1 = "BD_FPL/Data/play.csv"
-p2 = "BD_FPL/Data/teams.csv"
+p1 = "data/play.csv"
+p2 = "data/teams.csv"
 
 player_df_path, teams_df_path = p1, p2
 
-player_df = sqlContext.read.load(p1, format="com.databricks.spark.csv", header = True, inferSchema = True)
-teams_df = sqlContext.read.load(p2, format="com.databricks.spark.csv", header = True, inferSchema = True)
+player_df = sqlContext.read.load(
+    p1, format="com.databricks.spark.csv", header=True, inferSchema=True)
+teams_df = sqlContext.read.load(
+    p2, format="com.databricks.spark.csv", header=True, inferSchema=True)
 
 lines = ssc.socketTextStream("localhost", 6100)
-
-
-#quiet_logs(sc)
-
