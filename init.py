af01aee8639872a6ed66d59c1078f36b51aa13a4
Aditeya Baral <aditeya.baral@gmail.com>
2020-11-17 22:17:29 +0530

    add file reading

--- init.py
+++ init.py
@@ -1,16 +1,12 @@
 import sys
-import socket
-import requests
 import json
-import time
 import csv
-import cryptography
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext
 from pyspark.streaming import StreamingContext
 
 
-sc = SparkContext("local", "fpl")
+sc = spark.sparkContext #SparkContext("local", "fpl")
 sql_context = SQLContext(sc)
 ssc = StreamingContext(sc, 1)
 
@@ -19,9 +15,14 @@ p2 = "data/teams.csv"
 
 player_df_path, teams_df_path = p1, p2
 
-player_df = sqlContext.read.load(
-    p1, format="com.databricks.spark.csv", header=True, inferSchema=True)
-teams_df = sqlContext.read.load(
-    p2, format="com.databricks.spark.csv", header=True, inferSchema=True)
+#player_df = sqlContext.read.load(p1, format="com.databricks.spark.csv", header=True, inferSchema=True)
+#teams_df = sqlContext.read.load(p2, format="com.databricks.spark.csv", header=True, inferSchema=True)
+
+def func(x):
+    return json.loads(x)
 
 lines = ssc.socketTextStream("localhost", 6100)
+#lines.pprint()
+data = lines.map(lambda x: f"Keys = {list(json.loads(x).keys())}")
+data.pprint()
+ssc.start()
