7ee8ea0f2cf8198d2b17927ce6bc1f999461eb14
Aditeya Baral <aditeya.baral@gmail.com>
2020-11-20 19:08:02 +0530

    add function

--- init.py
+++ init.py
@@ -1,31 +1,39 @@
+import os
 import sys
 import json
 import csv
+import utils
 from pyspark import SparkContext, SparkConf
 from pyspark.sql import SQLContext
 from pyspark.streaming import StreamingContext
 
 #sc = spark.sparkContext
-sc = SparkContext(master="local[2]", appName="fpl")
+sc = SparkContext(master=f"local[{os.cpu_count()-1}]", appName="fpl")
 sql_context = SQLContext(sc)
 ssc = StreamingContext(sc, batchDuration=1)
 
-p1 = "data/play.csv"
+p1 = "data/players.csv"
 p2 = "data/teams.csv"
 
 player_df_path, teams_df_path = p1, p2
 
-#player_df = sqlContext.read.load(p1, format="com.databricks.spark.csv", header=True, inferSchema=True)
-#teams_df = sqlContext.read.load(p2, format="com.databricks.spark.csv", header=True, inferSchema=True)
+player_df = sql_context.read.load(p1, format="com.databricks.spark.csv", header=True, inferSchema=True)
+teams_df = sql_context.read.load(p2, format="com.databricks.spark.csv", header=True, inferSchema=True)
+
 
 def func(x):
-    data = json.loads(x)
-    return data
+    #print(type(x))
+    stream_data = json.loads(x)
+    #print(type(stream_data))
+    #print("################# HERE FUNC BEGINNING #################")
+    getPassAccuracy(player_df, stream_data)
+    #print("############ HERE AFTER UTILS ####################")
+    return stream_data
 
 lines = ssc.socketTextStream("localhost", 6100)
-lines.pprint()
-#data = lines.map(lambda x: func(x))
-
+data = lines.map(lambda x: func(x))
+#getPassAccuracy(player_df, data)
+data.pprint()
 ssc.start()
 ssc.awaitTermination()
-#ssc.stop()
+ssc.stop()
